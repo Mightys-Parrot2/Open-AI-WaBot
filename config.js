5baec80a15aad3550c5622c2cdd0515a143d8bbd
Mightys-Parrot2 <105868831+Mightys-Parrot2@users.noreply.github.com>
2023-02-15 15:24:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=JiYDiILA#Ogmf7-LuH8nYT3S4zG3HpNXEgY6ys6YC-Yu-QGjVkTA' : process.env.SESSION_ID
 };
